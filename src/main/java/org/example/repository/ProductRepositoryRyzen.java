632752a610d64b681355af39eb11eb75b765c711
AlvaroMendes <alvaro.afp.mendes@gmail.com>
2024-05-28 01:29:13 +0100

    Rolled back some mess

--- src/main/java/org/example/repository/ProductRepositoryRyzen.java
+++ src/main/java/org/example/repository/ProductRepositoryRyzen.java
@@ -1,9 +1,9 @@
 package org.example.repository;
 
-import org.example.model.ProductRyzenCPU;
+import org.example.model.ProductInterface;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<ProductRyzenCPU, Long> {
+public interface ProductRepository extends JpaRepository<ProductInterface, Long> {
 }
